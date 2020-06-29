560ad113f99b951b31acb515734b2ae46e505684
andrius-siup <andrius.siupinys@gmail.com>
2020-06-29 14:48:56 +0100

    Still trying

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        
+
+        Scanner input = new Scanner(System.in);
+        // input first number
+        System.out.print("Enter a number: ");
+        double number1 = input.nextDouble();
+
+        // input operator
+        System.out.print("Enter a operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);
+
+        // input a second number
+        System.out.print("Enter a second number: ");
+        double number2 = input.nextDouble();
+
+        double total;
+        switch (operator)
+        {
+            case '+':
+                total = number1 + number2;
+                System.out.println(total);
+                break;
+            case '-':
+                total = number1 - number2;
+                System.out.println(total);
+                break;
+            case '*':
+                total = number1 * number2;
+                System.out.println(total);
+                break;
+            case '/':
+                total = number1 / number2;
+                System.out.println(total);
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+                break;
+        }
+
+
+
     }
 }
