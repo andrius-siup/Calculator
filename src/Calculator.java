34092e31959d44b7f4f1e51b083e0948039ff779
andrius-siup <andrius.siupinys@gmail.com>
2020-07-01 19:44:53 +0100

    Added operator

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,6 +10,33 @@ public class Calculator {
 
         System.out.print("Enter a second number: ");
         double num2 = input.nextDouble();
-        
+
+        // Choose an operator
+        System.out.print("Enter an operator: (+, -, *, /) ");
+        char operator = input.next().charAt(0);
+
+        double total = 0.0;
+
+        switch (operator)
+        {
+            case '+':
+                total = num1 + num2;
+                System.out.println(total);
+                break;
+            case '-':
+                total = num1 - num2;
+                System.out.println(total);
+                break;
+            case '*':
+                total = num1 * num2;
+                System.out.println(total);
+                break;
+            case '/':
+                total = num1 / num2;
+                break;
+            default:
+                System.out.println("Error. Please enter a number: ");
+                break;
+        }
     }
 }
